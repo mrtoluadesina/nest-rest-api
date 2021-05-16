c04586620fe3b30b1d3a1e448ab73a6d4d87522b
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-16 03:30:04 +0100

    create api, add mongoDB, add swagger, and implement validations using pipes

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Rest API')
+    .setDescription('This is a sample nest js project')
+    .setVersion('1.0')
+    .build()
+    
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/documentation',app, document)
+
+  await app.listen(3000);
+}
+bootstrap();
